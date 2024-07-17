3dc8947ffe514c6ce366ecb3d55d883aa196c72f
bryceshirley <bryce.shirley@stfc.ac.uk>
2024-07-17 15:09:15 +0000

    Split docker files and copy results

--- Dockerfiles/build_containers.sh
+++ Dockerfiles/build_containers.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Build gpu_base image
+echo "Building gpu_base image..."
 sudo docker build -t gpu_base -f Dockerfile.gpu_base .
+
+# Build sciml_base image using gpu_base as base
+echo "Building sciml_base image..."
 sudo docker build -t sciml_base -f Dockerfile.sciml_base .
+
+## Build sciml benchmark containers
+
+# Build synthetic_regression image using sciml_base as base
+echo "Building synthetic_regression image..."
 sudo docker build -t synthetic_regression -f Dockerfile.synthetic_regression .
+
+# Build stemdl_classification image using sciml_base as base
+
+# Build slstr_cloud image using sciml_base as base
+
+echo -e "Build process completed.\n"
--- Dockerfiles/run_and_copy_results.sh
+++ Dockerfiles/run_and_copy_results.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
 
 # Define the image name and file path within the container
-USR="dnz75396"
+CURRENT_USR=$(whoami)
+CURRENT_HOST=$(hostname)
+HOST_AND_PATH="${CURRENT_USER}@${CURRENT_HOST}"
 IMAGE_NAME="synthetic_regression"
+CONTAINER_NAME="synthetic_regression"
 FILE_PATH="/root/gpu_benchmark_metrics/results"
-DESTINATION_PATH="/home/$USR/results_$IMAGE_NAME"
+DESTINATION_PATH="/home/$CURRENT_USR/Benchmark_Dockerfiles/results_$IMAGE_NAME"
 
-# Check if container is running
-if sudo docker ps -f "name=$IMAGE_NAME" --format '{{.Status}}' | grep -q "Up"; then
-    echo "Container $IMAGE_NAME is already running."
+# # Check if container is Up
+# if sudo docker ps -f "name=$IMAGE_NAME" --format '{{.Status}}' | grep -q "Up"; then
+#     echo "Container $IMAGE_NAME is already up."
+# else
+#     echo "Starting container $IMAGE_NAME..."
+#     sudo docker start "$IMAGE_NAME"
+
+#     # Wait for the container to be fully up
+#     sleep 5  # Adjust the sleep time if needed
+
+#     # Check if the container started successfully
+#     CONTAINER_STATUS=$(sudo docker ps -q -f name=$CONTAINER_NAME)
+#     if [ -z "$CONTAINER_STATUS" ]; then
+#         echo "Failed to start the container."
+#         exit 1
+#     else
+#         echo "Container $CONTAINER_NAME started successfully."
+#     fi
+# fi
+
+# Execute the command inside the container
+echo "Executing command inside the container..."
+sudo docker run --gpus all $IMAGE_NAME:latest /bin/bash -c "
+  source /root/anaconda3/bin/activate bench &&
+  cd /root/gpu_benchmark_metrics/ &&
+
+  # Run and Monitor Benchmark
+  ./monitor.sh $IMAGE_NAME --plot
+"
+
+# Check if the command executed successfully
+if [ $? -ne 0 ]; then
+    echo "Failed to execute the command inside the container."
+    exit 1
 else
-    echo "Starting container $IMAGE_NAME..."
-    sudo docker start "$IMAGE_NAME"
-    if [ $? -eq 0 ]; then
-        echo "Container $IMAGE_NAME started successfully."
-    else
-        echo "Failed to start container $IMAGE_NAME. Please check Docker logs."
-        exit 1
-    fi
+    echo "Command executed successfully."
 fi
-
-# Run Container
-sudo docker run --gpus all "$IMAGE_NAME"
 
 # Find the container ID by image name
 CONTAINER_ID=$(sudo docker ps -a --filter "ancestor=$IMAGE_NAME:latest" -q | head -n 1)
@@ -32,7 +56,6 @@ if [ -z "$CONTAINER_ID" ]; then
     exit 1
 fi
 
-# Copy the Results from the container to host
 echo "Copying files from container to host..."
 sudo docker cp "$CONTAINER_ID:$FILE_PATH/." "$DESTINATION_PATH"
 
@@ -42,8 +65,10 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Change Ownership and Permissions
-sudo chown -R $USR:wheel "$DESTINATION_PATH"
+# # Change Ownership and Permissions
+sudo chown -R $CURRENT_USR:wheel "$DESTINATION_PATH"
 sudo chmod -R u+rw "$DESTINATION_PATH"
+
+sudo docker rm $CONTAINER_ID
 
 echo "Files copied successfully from container to host."
