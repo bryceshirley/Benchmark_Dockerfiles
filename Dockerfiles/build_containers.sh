2632f25fa5a9353a80400fb76cafd007208195fd
bryceshirley <bryce.shirley@stfc.ac.uk>
2024-07-18 09:32:24 +0000

    Assume USR has added themselves to docker user group (see README)

--- Dockerfiles/build_containers.sh
+++ Dockerfiles/build_containers.sh
@@ -2,17 +2,17 @@
 
 # Build gpu_base image
 echo "Building gpu_base image..."
-sudo docker build -t gpu_base -f Dockerfile.gpu_base .
+docker build -t gpu_base -f Dockerfile.gpu_base .
 
 # Build sciml_base image using gpu_base as base
 echo "Building sciml_base image..."
-sudo docker build -t sciml_base -f Dockerfile.sciml_base .
+docker build -t sciml_base -f Dockerfile.sciml_base .
 
 ## Build sciml benchmark containers
 
 # Build synthetic_regression image using sciml_base as base
 echo "Building synthetic_regression image..."
-sudo docker build -t synthetic_regression -f Dockerfile.synthetic_regression .
+docker build -t synthetic_regression -f Dockerfile.synthetic_regression .
 
 # Build stemdl_classification image using sciml_base as base
 
--- Dockerfiles/run_and_copy_results.sh
+++ Dockerfiles/run_and_copy_results.sh
@@ -11,8 +11,9 @@ CONTAINER_NAME=$BENCHMARK
 FILE_PATH="/root/gpu_benchmark_metrics/results"
 DESTINATION_PATH="/home/$CURRENT_USR/Benchmark_Dockerfiles/results_$BENCHMARK"
 
+
 # Run the Benchmark in the container
-sudo docker run --name $CONTAINER_NAME --gpus all synthetic-regression:latest
+docker run --name $CONTAINER_NAME --gpus all $IMAGE_NAME:latest
 
 # Check if the command executed successfully
 if [ $? -ne 0 ]; then
@@ -23,30 +24,26 @@ else
 fi
 
 # Find the container ID by image name
-CONTAINER_ID=$(sudo docker ps -a --filter "name=$CONTAINER_NAME" --format "{{.ID}}")
+CONTAINER_ID=$(docker ps -a --filter "name=$CONTAINER_NAME" --format "{{.ID}}")
 
 # Check if container ID is found
 if [ -z "$CONTAINER_ID" ]; then
     echo "No container found for image $IMAGE_NAME."
-    sudo docker rm $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
     exit 1
 fi
 
 echo "Copying files from container to host..."
-sudo docker cp "$CONTAINER_ID:$FILE_PATH/." "$DESTINATION_PATH"
+docker cp "$CONTAINER_ID:$FILE_PATH/." "$DESTINATION_PATH"
 
 # Check if docker cp command executed successfully
 if [ $? -ne 0 ]; then
     echo "Failed to copy files from container to host."
-    sudo docker rm $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
     exit 1
 fi
 
-# Change Ownership and Permissions
-sudo chown -R $CURRENT_USR:wheel "$DESTINATION_PATH"
-sudo chmod -R u+rw "$DESTINATION_PATH"
-
 # Remove Container
-sudo docker rm $CONTAINER_NAME
+docker rm $CONTAINER_NAME
 
 echo "Files copied successfully from container to host."
