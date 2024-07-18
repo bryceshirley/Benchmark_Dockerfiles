c72b1b592b2804322ad7065f940939a303a31773
bryceshirley <bryce.shirley@stfc.ac.uk>
2024-07-18 11:16:26 +0000

    improved usage

--- build_images.sh
+++ build_images.sh
@@ -2,17 +2,17 @@
 
 # Build gpu_base image
 echo "Building gpu_base image..."
-docker build -t gpu_base -f Dockerfile.gpu_base .
+docker build -t gpu_base -f Dockerfiles/Dockerfile.gpu_base .
 
 # Build sciml_base image using gpu_base as base
 echo "Building sciml_base image..."
-docker build -t sciml_base -f Dockerfile.sciml_base .
+docker build -t sciml_base -f Dockerfiles/Dockerfile.sciml_base .
 
 ## Build sciml benchmark containers
 
 # Build synthetic_regression image using sciml_base as base
 echo "Building synthetic_regression image..."
-docker build -t synthetic_regression -f Dockerfile.synthetic_regression .
+docker build -t synthetic_regression -f Dockerfiles/Dockerfile.synthetic_regression .
 
 # Build stemdl_classification image using sciml_base as base
 
--- run_container.sh
+++ run_container.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-BENCHMARK="synthetic_regression"
+# Ensure a benchmark is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <benchmark>"
+    echo -e "\nAvailable benchmarks:\n   synthetic_regression\n" 
+    exit 1
+fi
+BENCHMARK="$1"
 
 # Define the image name and file path within the container
 CURRENT_USR=$(whoami)
@@ -15,12 +21,10 @@ DESTINATION_PATH="/home/$CURRENT_USR/Benchmark_Dockerfiles/results_$BENCHMARK"
 # Run the Benchmark in the container
 docker run --name $CONTAINER_NAME --gpus all $IMAGE_NAME:latest
 
-# Check if the command executed successfully
-if [ $? -ne 0 ]; then
-    echo "Failed to execute the command inside the container."
+# Check if the container exists
+if ! docker ps -a --filter name=$CONTAINER_NAME --format '{{.Names}}' | grep -q $CONTAINER_NAME; then
+    echo "Container failed to start."
     exit 1
-else
-    echo "Command executed successfully."
 fi
 
 # Find the container ID by image name
